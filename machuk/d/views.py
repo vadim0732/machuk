ac0b2612172de63f5a5fa6e2cd42b90e9e506068
Студент группы IS <is@sielom.local>
2025-10-15 10:14:57 +0500

    urls_views

--- machuk/d/views.py
+++ machuk/d/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+
+    users = User.username
+
+    return render(request, 'index.html')
